16701cf8f64935067fab2c37a20cbe22c028d9e7
aml <amlmusa1357@gmail.com>
2022-06-29 08:16:00 +0200

    final

--- lib/presentation/widgets/character_item.dart
+++ lib/presentation/widgets/character_item.dart
@@ -1,4 +1,5 @@
 import 'package:breaking_bad_project/constants/colors.dart';
+import 'package:breaking_bad_project/constants/strings.dart';
 import 'package:breaking_bad_project/data/model/character.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,35 +16,38 @@ class CharacterItem extends StatelessWidget {
       padding: EdgeInsets.all(4),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8), color: MyColors.myWhite),
-      child: GridTile(
-        child: Container(
-            child: character.image.isNotEmpty
-                ? FadeInImage.assetNetwork(
-                    placeholder: 'assets/images/placeHolder.gif',
-                    image: character.image,
-                    fit: BoxFit.cover,
-                  )
-                : Center(
-                    child: Icon(
-                      Icons.person_pin_rounded,
-                      size: 60,
-                    ),
-                  )),
-        footer: Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          color: Colors.black54,
-          alignment: Alignment.bottomCenter,
-          child: Text(
-            "${character.name}",
-            style: TextStyle(
-                height: 1.3,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: MyColors.myWhite),
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            textAlign: TextAlign.center,
+      child: InkWell(
+        onTap: ()=> Navigator.pushNamed(context,characterDetailsScreen ,arguments: character),
+        child: GridTile(
+          child: Container(
+              child: character.image.isNotEmpty
+                  ? FadeInImage.assetNetwork(
+                      placeholder: 'assets/images/placeHolder.gif',
+                      image: character.image,
+                      fit: BoxFit.cover,
+                    )
+                  : Center(
+                      child: Icon(
+                        Icons.person_pin_rounded,
+                        size: 60,
+                      ),
+                    )),
+          footer: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            color: Colors.black54,
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              "${character.name}",
+              style: TextStyle(
+                  height: 1.3,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: MyColors.myWhite),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
